a7649dfd473725f94c6d255fbae69a91ee8b8046
yuhuei <maggie19921001@gmail.com>
2024-10-09 15:15:05 +0800

    login page base setting

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +12,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(VueAxios, axios)
 app.mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from "vue";
+
+const email = ref('');
+const password = ref('');
+const loginData = ref({
+  email: email,
+  password: password,
+})
+</script>
+<template>
+    <div class="container mt-5">
+      <form class="row justify-content-center"
+        >
+        <div class="col-md-6">
+          <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
+          <div class="mb-2">
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input
+              type="email"
+              id="inputEmail"
+              class="form-control"
+              placeholder="Email address"
+              required
+              autofocus
+              
+            />
+          </div>
+          <div class="mb-2">
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input
+              type="password"
+              id="inputPassword"
+              class="form-control"
+              placeholder="Password"
+              required
+              
+            />
+          </div>
+          <div class="text-end mt-4">
+            <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </template>
