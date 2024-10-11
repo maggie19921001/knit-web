418d876b043b5f2e376865bc9d848aa426affee8
yuhuei <maggie19921001@gmail.com>
2024-10-11 16:32:39 +0800

    Updates

---
+++ assets/Dashboard-BUn-51Eb.js
@@ -0,0 +1,1 @@
+import{r as o,d as a,u as s}from"./index-B0p8xzY6.js";const c={__name:"Dashboard",setup(r){s();const e=o(document.cookie.replace(/(?:(?:^|.*;\s*)loginToken\s*=\s*([^;]*).*$)|^.*$/,"$1"));return a.defaults.headers.common.Authorization=e.value,(t,n)=>" Dashboard "}};export{c as default};
