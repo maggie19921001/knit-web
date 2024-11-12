6c4fc5f1a89c7bb57a5f7ca6a11c5ac56589b615
yuhuei <maggie19921001@gmail.com>
2024-11-12 17:10:26 +0800

    Updates

--- assets/component-functions-Cshj2gzD.js
+++ assets/component-functions-Cshj2gzD.js
@@ -1,4 +1,4 @@
-import{y as N}from"./index-BkuwJpVc.js";var K={exports:{}},j={exports:{}};/*!
+import{y as N}from"./index-2Dn1kYt-.js";var K={exports:{}},j={exports:{}};/*!
   * Bootstrap data.js v5.3.3 (https://getbootstrap.com/)
   * Copyright 2011-2024 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
