a3498dcbbccea08c47b00b0568448a974ab02ab8
yuhuei <maggie19921001@gmail.com>
2024-11-12 14:42:49 +0800

    移除初始檔案

--- src/router/index.js
+++ src/router/index.js
@@ -9,14 +9,6 @@ const router = createRouter({
       component: () => import('../views/HomeView.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/login',
       name: 'login',
       component: () => import('../views/admin/Login.vue')
